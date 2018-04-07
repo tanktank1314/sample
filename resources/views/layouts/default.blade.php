89b74c4bd083b4e2188fa327eac252a6d25aee94
Tank <1366453994@qq.com>
2018-04-07 08:41:20 +0000

    Finish static pages

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="content-type" content="text/html;charset=utf-8">
+    <title>@yield('title','Sample') - Laravel 新手入门教程</title>
+    <link rel="stylesheet" href="">
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
