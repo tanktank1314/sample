f91e8a2d6863bea9b7262556fa50aebfbd004e03
Tank <1366453994@qq.com>
2018-04-08 10:12:17 +0800

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,9 +3,15 @@
 <head>
     <meta name="content-type" content="text/html;charset=utf-8">
     <title>@yield('title','Sample') - Laravel 新手入门教程</title>
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-    @yield('content')
+    @include('layouts._header')
+    <div class="container">
+        <div class="col-md-offset-1 col-md-10">
+            @yield('content')
+            @include('layouts._footer')
+        </div>
+    </div>
 </body>
 </html>
