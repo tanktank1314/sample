eae282dd21f3f78603796a7e14ce62525e26ad7e
Tank <1366453994@qq.com>
2018-04-09 15:00:20 +0800

    Add account activations

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -20,6 +20,7 @@ class UsersTableSeeder extends Seeder
         $user->email = '1366453994@qq.com';
         $user->password = bcrypt('28375930');
         $user->is_admin = true;
+        $user->activated = true;
         $user->save();
     }
 }
