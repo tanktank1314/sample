cacdf81a5e981f8ddd0a48f2f04c84247ba101a0
Tank <1366453994@qq.com>
2018-04-09 21:34:40 +0800

    Add user statuses

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use App\Notifications\ResetPassword;
+use App\Models\Status;
 
 class User extends Authenticatable
 {
@@ -48,5 +49,16 @@ class User extends Authenticatable
     public function sendPasswordResetNotification($token)
     {
         $this->notify(new ResetPassword($token));
+    }
+
+    public function statuses()
+    {
+        return $this->hasMany(Status::class);
+    }
+
+    public function feed()
+    {
+        return $this->statuses()
+                        ->orderBy('created_at','desc');
     }
 }
