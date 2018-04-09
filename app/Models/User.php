97108408ce21e36df4bd2cc099623bf16f0c4a00
Tank <1366453994@qq.com>
2018-04-09 13:20:32 +0800

    Finish user edit,update,index,and destroy actions

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,7 +29,7 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
-    public function gravater($size = '100')
+    public function gravatar($size = '100')
     {
         $hash = md5(strtolower(trim($this->attributes['email'])));
         return "http://www.gravatar.com/avatar/$hash?s=$size";
--- resources/views/shared/_user_info.blade.php
+++ resources/views/shared/_user_info.blade.php
@@ -1,4 +1,4 @@
 <a href="{{ route('users.show',$user) }}">
-    <img src="{{ $user->gravater('140') }}" alt="{{ $user->name }}" class="gravater">
+    <img src="{{ $user->gravatar('140') }}" alt="{{ $user->name }}" class="gravatar">
 </a>
 <h1>{{ $user->name }}</h1>
